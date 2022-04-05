8ed9adeee2fff2dc4f724423b2bb29d2f44cb40b
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-04-05 15:21:42 -0500

    StartingWorking

---
+++ Game Sim 2 Project 3/Assets/PlayerAttributes.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    public float fuel;
+
+    public Vector2 position;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game Sim 2 Project 3/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public void MovePlayer()
+    {
+        // player.velocity.x += player.accell.x * DeltaTime;
+        // player.velocity.y += player.accell.y * DeltaTime;
+        // player.position.x += player.velocity.x * DeltaTime;
+        // player.position.y += player.velocity.y * DeltaTime;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
