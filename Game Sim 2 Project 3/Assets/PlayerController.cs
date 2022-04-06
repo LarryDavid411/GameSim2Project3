792dd89243670a27badb0a3124c6b5611cadaedf
LarryDavid411 <65431559+LarryDavid411@users.noreply.github.com>
2022-04-06 13:16:23 -0500

    Right Accell working

--- Game Sim 2 Project 3/Assets/PlayerController.cs
+++ Game Sim 2 Project 3/Assets/PlayerController.cs
@@ -1,22 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
     public float fuel;
-    public float accelleration;
+    private Vector2 accelleration;
+    private Vector3 velocity;
 
     //public Vector2 position;
     private float movementTimerRight;
     private float movementTimerLeft;
     private float movementTimerUp;
     private float movementTimerDown;
+    
+    public float movementDuration;
 
     public void MovePlayer()
     {
         //float z = Input.GetAxis("Vertical");
         //float x = Input.GetAxis("Horizontal");  
+        velocity.x += accelleration.x * Time.deltaTime;
+        velocity.y += accelleration.y * Time.deltaTime;
+        Vector3 movePlayerPosition = new Vector2();
+        movePlayerPosition.x = velocity.x * Time.deltaTime;
+        movePlayerPosition.y = velocity.y * Time.deltaTime;
+        this.gameObject.transform.position += movePlayerPosition;
         
         if (fuel > 0)
         {
@@ -24,48 +37,54 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 movementTimerRight += Time.deltaTime;
-                //Debug.Log("RightArrow");
+                float time = movementTimerRight / movementDuration;
+                //Vector3 lerp = Vector3.Lerp(0f, 20f, time);
+                accelleration.x = Mathf.Lerp(0f, 20f, time);
+                Debug.Log(accelleration.x);
             }
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 movementTimerLeft += Time.deltaTime;
+                float time = movementTimerLeft / movementDuration;
+               
                //Debug.Log("LeftArrow");
             }
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 movementTimerUp += Time.deltaTime;
+                float time = movementTimerUp / movementDuration;
+                
                 //Debug.Log("UpArrow");
             }
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 movementTimerDown += Time.deltaTime;
+                float time = movementTimerDown / movementDuration;
+                
                 //Debug.Log("DownArrow");
             }
             
             // Key Released
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKeyUp(KeyCode.RightArrow))
             {
-                movementTimerRight = 0;
+               movementTimerRight = 0;
             }
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
             {
                 movementTimerLeft = 0;
             }
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKeyUp(KeyCode.UpArrow))
             {
                 movementTimerUp = 0;
             }
-            if (Input.GetKey(KeyCode.DownArrow))
+            if (Input.GetKeyUp(KeyCode.DownArrow))
             {
                 movementTimerDown = 0;
             }
              
         }
         
-        // player.velocity.x += player.accell.x * DeltaTime;
-        // player.velocity.y += player.accell.y * DeltaTime;
-        // player.position.x += player.velocity.x * DeltaTime;
-        // player.position.y += player.velocity.y * DeltaTime;
+        
     }
     // Start is called before the first frame update
     void Start()
